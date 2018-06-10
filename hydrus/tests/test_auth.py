b5c6691b8d455b28f5e9807a3ee73fe932c760b0
Vishal Desai <vddesai1998@gmail.com>
2018-06-10 19:00:36 +0530

    Adapted authentication tests

--- hydrus/tests/test_auth.py
+++ hydrus/tests/test_auth.py
@@ -4,7 +4,7 @@ from hydrus.data.user import generate_basic_digest
 import json
 import unittest
 from hydrus.app import app_factory
-from hydrus.utils import set_session, set_doc, set_api_name, set_authentication
+from hydrus.utils import Getter_setter
 from hydrus.data import doc_parse
 from hydrus.hydraspec import doc_writer_sample, doc_maker
 from sqlalchemy import create_engine
@@ -12,17 +12,18 @@ from sqlalchemy.orm import sessionmaker,scoped_session
 from hydrus.data.db_models import Base
 from hydrus.data.user import add_user
 from base64 import b64encode
+from falcon import testing
 
 
 # response = requests.get("http://127.0.0.1:8080/serverapi/CommandCollection", headers={'Authorization':'Basic QWxhZGRpbjpPcGVuU2VzYW1l'})
 
 
-class AuthTestCase(unittest.TestCase):
+class AuthTestCase(testing.TestCase):
     """Test Class for the app."""
 
-    @classmethod
-    def setUpClass(self):
+    def setUp(self):
         """Database setup before the tests."""
+        super(AuthTestCase, self).setUp()
         print("Creating a temporary datatbase...")
         engine = create_engine('sqlite:///:memory:')
         Base.metadata.create_all(engine)
@@ -31,116 +32,98 @@ class AuthTestCase(unittest.TestCase):
         self.session = session
         self.API_NAME = "demoapi"
         self.HYDRUS_SERVER_URL = "http://hydrus.com/"
-        self.app = app_factory(self.API_NAME)
         self.doc = doc_maker.create_doc(doc_writer_sample.api_doc.generate(), self.HYDRUS_SERVER_URL, self.API_NAME)
-
+        self.gs = Getter_setter(self.session, self.HYDRUS_SERVER_URL, self.API_NAME, self.doc, True)
+        self.app = app_factory(self.API_NAME, self.gs)
         test_classes = doc_parse.get_classes(self.doc.generate())
         test_properties = doc_parse.get_all_properties(test_classes)
         doc_parse.insert_classes(test_classes, self.session)
         doc_parse.insert_properties(test_properties, self.session)
         add_user(1, "test", self.session)
-        self.auth_header = {"Authorization": "Basic " + b64encode(b"1:test").decode("ascii")}
-        self.wrong_id = {"Authorization": "Basic " + b64encode(b"2:test").decode("ascii")}
-        self.wrong_pass = {"Authorization": "Basic " + b64encode(b"1:test2").decode("ascii")}        
+        self.auth_header = {"Authorization": "Basic " + b64encode(b"1:test").decode("utf-8")}
+        self.wrong_id = {"Authorization": "Basic " + b64encode(b"2:test").decode("utf-8")}
+        self.wrong_pass = {"Authorization": "Basic " + b64encode(b"1:test2").decode("utf-8")}
         print("Classes, Properties and Users added successfully.")
 
         print("Setting up Hydrus utilities... ")
-        self.api_name_util =  set_api_name(self.app, self.API_NAME)
-        self.session_util = set_session(self.app, self.session)
-        self.doc_util = set_doc(self.app, self.doc)
-        self.auth_util = set_authentication(self.app, True)
-        self.client = self.app.test_client()
         
         print("Creating utilities context... ")
-        self.api_name_util.__enter__()
-        self.session_util.__enter__()
-        self.doc_util.__enter__()
-        self.auth_util.__enter__()
-        self.client.__enter__()
 
         print("Setup done, running tests...")
 
 
-    @classmethod
-    def tearDownClass(self):
-        """Tear down temporary database and exit utilities"""
-        self.client.__exit__(None, None, None)
-        self.auth_util.__exit__(None, None, None)
-        self.doc_util.__exit__(None, None, None)
-        self.session_util.__exit__(None, None, None)
-        self.api_name_util.__exit__(None, None, None)
-        self.session.close()
+class TestCases(AuthTestCase):
 
     def test_wrongID_GET(self):
         """Test for the index."""
-        response_get = self.client.get("/"+self.API_NAME)
-        endpoints = json.loads(response_get.data.decode('utf-8'))
+        response_get = self.simulate_get("/"+self.API_NAME)
+        endpoints = response_get.json
         for endpoint in endpoints:
             if endpoint in self.doc.collections:
-                response_get = self.client.get(endpoints[endpoint], headers=self.wrong_id)
+                response_get = self.simulate_get(endpoints[endpoint], headers=self.wrong_id)
                 assert response_get.status_code == 401 or response_get.status_code == 400
 
     def test_wrongID_POST(self):
         """Test for the index."""
-        response_get = self.client.get("/"+self.API_NAME)
-        endpoints = json.loads(response_get.data.decode('utf-8'))
+        response_get = self.simulate_get("/"+self.API_NAME)
+        endpoints = response_get.json
         for endpoint in endpoints:
             if endpoint in self.doc.collections:
-                response_get = self.client.post(endpoints[endpoint], headers=self.wrong_id, data=json.dumps(dict(foo="bar")))
+                response_get = self.simulate_post(endpoints[endpoint], headers=self.wrong_id, json=dict(foo="bar"))
                 assert response_get.status_code == 401 or response_get.status_code == 400
 
     def test_wrongPass_GET(self):
         """Test for the index."""
-        response_get = self.client.get("/"+self.API_NAME)
-        endpoints = json.loads(response_get.data.decode('utf-8'))
+        response_get = self.simulate_get("/"+self.API_NAME)
+        endpoints = response_get.json
         for endpoint in endpoints:
             if endpoint in self.doc.collections:
-                response_get = self.client.get(endpoints[endpoint], headers=self.wrong_pass)
+                response_get = self.simulate_get(endpoints[endpoint], headers=self.wrong_pass)
                 assert response_get.status_code == 401
 
     def test_wrongPass_POST(self):
         """Test for the index."""
-        response_get = self.client.get("/"+self.API_NAME)
-        endpoints = json.loads(response_get.data.decode('utf-8'))
+        response_get = self.simulate_get("/"+self.API_NAME)
+        endpoints = response_get.json
         for endpoint in endpoints:
             if endpoint in self.doc.collections:
-                response_get = self.client.post(endpoints[endpoint], headers=self.wrong_pass, data=json.dumps(dict(foo="bar")))
+                response_get = self.simulate_post(endpoints[endpoint], headers=self.wrong_pass, json=dict(foo="bar"))
                 assert response_get.status_code == 401
 
     def test_Auth_GET(self):
         """Test for the index."""
-        response_get = self.client.get("/"+self.API_NAME)
-        endpoints = json.loads(response_get.data.decode('utf-8'))
+        response_get = self.simulate_get("/"+self.API_NAME)
+        endpoints = response_get.json
         for endpoint in endpoints:
             if endpoint in self.doc.collections:
-                response_get = self.client.get(endpoints[endpoint], headers=self.auth_header)
+                response_get = self.simulate_get(endpoints[endpoint], headers=self.auth_header)
                 assert response_get.status_code != 401
 
     def test_Auth_POST(self):
         """Test for the index."""
-        response_get = self.client.get("/"+self.API_NAME)
-        endpoints = json.loads(response_get.data.decode('utf-8'))
+        response_get = self.simulate_get("/"+self.API_NAME)
+        endpoints = response_get.json
         for endpoint in endpoints:
             if endpoint in self.doc.collections:
-                response_get = self.client.post(endpoints[endpoint], headers=self.auth_header, data=json.dumps(dict(foo="bar")))
+                response_get = self.simulate_post(endpoints[endpoint], headers=self.auth_header, json=dict(foo="bar"))
                 assert response_get.status_code != 401
 
     def test_Auth_PUT(self):
         """Test for the index."""
-        response_get = self.client.get("/"+self.API_NAME)
-        endpoints = json.loads(response_get.data.decode('utf-8'))
+        response_get = self.simulate_get("/"+self.API_NAME)
+        endpoints = response_get.json
         for endpoint in endpoints:
             if endpoint in self.doc.collections:
-                response_get = self.client.put(endpoints[endpoint], headers=self.auth_header, data=json.dumps(dict(foo="bar")))
+                response_get = self.simulate_put(endpoints[endpoint], headers=self.auth_header, json=dict(foo="bar"))
                 assert response_get.status_code != 401
 
     def test_Auth_DELETE(self):
         """Test for the index."""
-        response_get = self.client.get("/"+self.API_NAME)
-        endpoints = json.loads(response_get.data.decode('utf-8'))
+        response_get = self.simulate_get("/"+self.API_NAME)
+        endpoints = response_get.json
         for endpoint in endpoints:
             if endpoint in self.doc.collections:
-                response_get = self.client.delete(endpoints[endpoint], headers=self.auth_header)
+                response_get = self.simulate_delete(endpoints[endpoint], headers=self.auth_header)
                 assert response_get.status_code != 401
 
 
